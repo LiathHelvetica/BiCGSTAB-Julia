7e2cfae57ab3b653562b5a38121544a897ece9c2
Ola Kowalczyk <56888996+Olakow@users.noreply.github.com>
2023-06-12 23:29:39 +0100

    dodanie optymalizacji do modified_incomplete_cholesky.jl

--- modified_incomplete_cholesky.jl
+++ modified_incomplete_cholesky.jl
@@ -1,56 +1,54 @@
 using LinearAlgebra, SparseArrays
+using SuiteSparse
+import IncompleteLU: ILUFactorization
 
 function modified_incomplete_cholesky(A::SparseMatrixCSC{T, TID})::ILUFactorization{T, TID} where {T <: Number, TID <: Integer}
     n = size(A, 1)
     L = spzeros(T, n, n)
     ffj = 0.01
+    sqrt_Ajj = sqrt(A[1, 1])  # LICZYMY POZA PĘTLĄ
 
-    
+
+    w = similar(L, n)  # PRELOKACJA POZA PĘTLĄ
+
     for j = 1:n
-        L[j, j] = sqrt(A[j, j])
-        w = zeros(T, n)
-
-        w1_j = zeros(T, j)
-        wj_1_n = A[j+1:end, j]
-        w = [w1_j; wj_1_n]
+        L[j, j] = sqrt_Ajj
+        
+        @views w[1:j] .= 0
+        @views w[j+1:end] .= A[j+1:end, j]
         
         for k = 1:j-1
-           
-            
-            for i = j+1:n
-
-                if L[j, k] != zero(T)
-                    w[i] -= L[i, k] * L[j, k]
-                end
+            if L[j, k] == zero(T)
+                continue
             end
+
+            @views w[j+1:n] .-= L[j, k] * L[k, j+1:n]
         end
-        
+
         for i = j+1:n
             w[i] /= L[j, j]
         end
-        
-        ffj = ffj * norm(w,2)
-       
+
+        ffj *= norm(w, 2)
+
         for i = j+1:n
             if abs(w[i]) < ffj
                 w[i] = zero(T)
             end
         end
-        
-        
+
         p = Int(floor(maximum(A[:, j])))
 
+      #  stosujemy żeby zaoszczędzić pamięć ale zwiększamy czas - zależy czego oczekujemy
+      #  @inbounds for k = 1:p
         for k = 1:p
-            
-            L[k, j] -= w[k]          
-        end       
-
-            
-        for i = j+1:n
-            A[i, i] -= L[i,j]^2          
+            L[k, j] -= w[k]
         end
 
-
+       # @inbounds for i = j+1:n
+        for i = j+1:n
+            A[i, i] -= L[i, j]^2
+        end
     end
 
     return ILUFactorization(A, L)
