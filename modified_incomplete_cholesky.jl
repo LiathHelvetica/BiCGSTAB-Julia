166ac9f354d39299b65f4d3641a74676fdeafc6e
Ola Kowalczyk <56888996+Olakow@users.noreply.github.com>
2023-06-15 22:47:01 +0100

    Update modified_incomplete_cholesky.jl

--- modified_incomplete_cholesky.jl
+++ modified_incomplete_cholesky.jl
@@ -3,19 +3,19 @@ using LinearAlgebra, SparseArrays
 function modified_incomplete_cholesky(A::SparseMatrixCSC{T, TID})::ILUFactorization{T, TID} where {T <: Number, TID <: Integer}
     n = size(A, 1)
     L = spzeros(T, n, n)
-    ffj = 0.01
+    ffj = 0.001
     A_copy = copy(A)
 
     
-    for j = 1:n
+    @inbounds for j = 1:n
         L[j, j] = sqrt(A_copy[j, j])
         w = spzeros(T, n)
 
         w1_j = spzeros(T, j)
         wj_1_n = A_copy[j+1:end, j]
-        w = [w1_j; wj_1_n]
+        w =  [w1_j; wj_1_n]
         
-        for k = 1:j-1
+        @inbounds for k = 1:j-1
            
             
             for i = j+1:n
@@ -26,13 +26,13 @@ function modified_incomplete_cholesky(A::SparseMatrixCSC{T, TID})::ILUFactorizat
             end
         end
         
-        for i = j+1:n
-            w[i] /= L[j, j]
+        @inbounds for i = j+1:n
+            w[i] /=  L[j, j]
         end
         
         ffj = ffj * norm(w,2)
        
-        for i = j+1:n
+        @inbounds for i = j+1:n
             if abs(w[i]) < ffj
                 w[i] = zero(T)
             end
@@ -41,13 +41,13 @@ function modified_incomplete_cholesky(A::SparseMatrixCSC{T, TID})::ILUFactorizat
         
         p = Int(floor(maximum(A_copy[:, j])))
 
-        for k = 1:p
+        @inbounds for k = 1:p
             
-            L[k, j] -= w[k]          
+            L[k, j] -=  w[k]          
         end       
 
             
-        for i = j+1:n
+        @inbounds for i = j+1:n
             A_copy[i, i] -= L[i,j]^2          
         end
 
